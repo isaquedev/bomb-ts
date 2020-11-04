6ee154333968debec37cd309d6cad981cef1b530
Isaque Hernandes <isaque.hernandes@chefsclub.com.br>
2020-11-04 03:12:22 -0300

    configure heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen('3000');
+app.listen(PORT);
